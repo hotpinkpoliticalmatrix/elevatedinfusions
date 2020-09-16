ac487356f81c18867b170f2d434b56cf36ccb880
Stacey Eliuk <stacey.eliuk@gmail.com>
2020-09-15 23:50:00 -0400

    style: menu positioning

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,8 +3,7 @@ import GridItem from "./GridItem"
 import GridContainer from "./GridContainer"
 import withStyles from "@material-ui/core/styles/withStyles"
 import classNames from "classnames"
-import menuTitle from "../images/branding/menu.png"
-import logo from "../images/branding/logoBlue.png"
+import logo from "../images/branding/menuTitle.png"
 import menu from "../images/mainProducts/menu.jpg"
 import profilePageStyle from "../styles/material-kit-react/views/profilePage"
 
@@ -12,12 +11,13 @@ function Menu({ classes }) {
   return (
     <GridContainer className={classes.menuContainer} justify="center">
       <GridItem xs={12} sm={12} md={4}>
-        <img src={logo} alt="logo" />
-        <img src={menuTitle} className={classes.menuTitle} alt="menuTitle" />
+        <div className={classes.menuTitle}>
+          <img id="menu" src={logo} alt="logo" />
+        </div>
       </GridItem>
       <GridItem xs={12} sm={12} md={8}>
-        <div>
-          <img id="menu" className={classes.menu} src={menu} alt="menu" />
+        <div className={classes.menuDiv}>
+          <img src={menu} className={classes.menu} alt="menu" />
         </div>
       </GridItem>
     </GridContainer>
--- src/styles/material-kit-react/views/profilePage.jsx
+++ src/styles/material-kit-react/views/profilePage.jsx
@@ -36,7 +36,7 @@ const profilePageStyle = {
   },
   mainRaised: {
     margin: "-5px 30px 0px",
-    borderRadius: "6px",
+    borderRadius: "10px 10px 0px 0px",
     boxShadow:
       "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)",
   },
@@ -72,21 +72,21 @@ const profilePageStyle = {
     },
   },
   menuTitle: {
-    textAlign: "center",
-    "& img": {
-      maxWidth: "200px",
-      // width: "100%",
+    "@media (min-width: 960px)": {
+      paddingTop: "250px",
+      margin: "0px 0px 0px 20px",
+      width: "105%",
       height: "auto",
-      marginBottom: "-50px",
-      transform: "translate3d(0, -50%, 0)",
     },
   },
   menuContainer: {
     backgroundColor: warningColor,
     margin: "0px",
   },
+  menuDiv: {
+    margin: "100px 40px",
+  },
   menu: {
-    marginBottom: "10px",
     borderRadius: "10%",
   },
 }
