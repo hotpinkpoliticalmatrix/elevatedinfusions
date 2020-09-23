9ed2a395f8f28550607b185fe9e14333bc3fd32c
Stacey Eliuk <stacey.eliuk@gmail.com>
2020-09-23 18:54:36 -0400

    config: changed offline configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,14 +98,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        workboxConfig: {
-          globPatterns: ["**/favicon.png"],
-        },
-        precachePages: [`/`],
-      },
-    },
+    `gatsby-plugin-offline`,
   ],
 }
